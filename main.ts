4992c77faa8706c674c76f41b2aa5f53d4d98c77
baitiankanbujian888 <45777149+baitiankanbujian888@users.noreply.github.com>
2018-12-11 10:57:30 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,46 +1,28 @@
-/**
- * makecode BH1750 Digital Ambient Light Sensor Package.
- * From microbit/micropython Chinese community.
- * http://www.micropython.org.cn
- */
 
 
 /**
- * BH1750 Digital Ambient Light Sensor Package
+ * DLM COMMU
  */
 //% weight=100 color=#000011 icon="\uf185"
-namespace BH1750 {
+namespace COMMU {
   
-    //% blockId="BH1750_INIT" block="set Address %addr"
+    //% blockId="COMMU_INIT" block="INIT"
     //% weight=100 blockGap=8
 
     export function init(): void {
         serial.redirect(SerialPin.P0, SerialPin.P1, 115200)
     }
 
-  
-
-    /**
-     * turn on BH1750.
-     */
-    //% blockId="BH1750_ON" block="turn on"
-    //% weight=90 blockGap=8
+ 
    
-    /**
-     * turn off BH1750, to reduce power consumption.
-     */
-    //% blockId="BH1750_OFF" block="turn off"
+    //% blockId="COMMU_EXIT" block="EIXT"
     //% weight=90 blockGap=8
-
 	export function exit(): void {
         serial.redirectToUSB()
     }
 
 
-    /**
-     * get ambient light data (lx)
-     */
-    //% blockId="BH1750_GET_INTENSITY" block="get intensity (lx)"
+    //% blockId="COMMU_TRANSFER" block="TRANSFER"
     //% weight=80 blockGap=8
 	export function transfer(): void {
         serial.writeString("motor")
